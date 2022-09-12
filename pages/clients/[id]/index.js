3a7732fe8072301155b633c1ac78a21c9b19b6d3
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-09-12 21:30:22 +0600

    Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,19 @@ function ClientsProjectPage() {
   const router = useRouter();
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // router.push("/clients/max/project-a");
+    router.push({
+      pathname: "/clients/[id]/[clientsprojectid]",
+      query: { id: "max", clientsprojectid: "project-a" },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
